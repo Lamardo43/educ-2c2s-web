474edb772d9e39238627bb0db9641e10db58e524
Klim Medvedev <dater01@mail.ru>
2024-06-19 12:00:30 +0300

    Some refs

--- exam/app/books.py
+++ exam/app/books.py
@@ -11,32 +11,28 @@ from app import db_connector
 
 bp = Blueprint('books', __name__, url_prefix='/books')
 
-MAX_PER_PAGE = 8
+MAX_PAGE_COUNT = 8
 
 
 def save_cover(background_image):
     if background_image:
-        md5_hash = hashlib.md5(background_image.read()).hexdigest()
-        mime_type = background_image.mimetype
+        hash = hashlib.md5(background_image.read()).hexdigest()
 
         try:
             with db_connector.connect().cursor(named_tuple=True, buffered=True) as cursor:
-                cursor.execute("SELECT id FROM covers WHERE filename=%s", (md5_hash + ".png",))
+                cursor.execute("SELECT id FROM covers WHERE filename=%s", (hash + ".png",))
                 count = cursor.fetchone()
 
-                if count is not None:
+                if count:
                     cover_id = count.id
                 else:
                     cursor.execute("INSERT INTO covers (mime_type, filename) VALUES (%s, %s)",
-                                   (mime_type, f'{md5_hash}.png'))
+                                   (background_image.mimetype, f'{hash}.png'))
                     cover_id = cursor.lastrowid
 
                     db_connector.connect().commit()
-            filename = f"{md5_hash}.png"
-
-            file_path = os.path.join('static/images', filename)
             background_image.seek(0)
-            background_image.save(file_path)
+            background_image.save(f"D:/Education/Education4/web/exam/app/static/images/{hash}.png")
 
             return cover_id
         except Exception as e:
@@ -46,18 +42,11 @@ def save_cover(background_image):
     return None
 
 
-def insert_book_genres(book_id, genres_selected):
-    with db_connector.connect().cursor(named_tuple=True, buffered=True) as cursor:
-        cursor.executemany("INSERT IGNORE INTO book_genres (book_id, genre_id) VALUES (%s, %s)",
-                           [(book_id, genre_id) for genre_id in genres_selected])
-        db_connector.connect().commit()
-
-
 @bp.route('/')
 @login_required
 def index():
     page = int(request.args.get('page', 1))
-    offset = (page - 1) * MAX_PER_PAGE
+    offset = (page - 1) * MAX_PAGE_COUNT
 
     with db_connector.connect().cursor(named_tuple=True, buffered=True) as cursor:
         cursor.execute("SELECT COUNT(*) as count FROM books")
@@ -69,15 +58,16 @@ def index():
                        "(SELECT ROUND(AVG(r.rating), 1) FROM reviews r WHERE r.book_id = b.id) AS avg_rating, "
                        "(SELECT COUNT(r.id) FROM reviews r WHERE r.book_id = b.id) AS review_count "
                        "FROM books b LEFT JOIN covers c ON b.cover_id = c.id ORDER BY b.year DESC LIMIT %s OFFSET %s",
-                       (MAX_PER_PAGE, offset))
+                       (MAX_PAGE_COUNT, offset))
         books = cursor.fetchall()
 
     books = [book._asdict() for book in books]
 
-    page_count = math.ceil(total_books / MAX_PER_PAGE)
-    pages = range(1, page_count + 1)
-
-    return render_template('books/index.html', books=books, pages=pages, page=page, page_count=page_count)
+    return render_template('books/index.html',
+                           books=books,
+                           pages=range(1, math.ceil(total_books / MAX_PAGE_COUNT) + 1),
+                           page=page,
+                           page_count=math.ceil(total_books / MAX_PAGE_COUNT))
 
 
 @bp.route('/create', methods=['GET', 'POST'])
@@ -86,20 +76,26 @@ def create():
     fields = ['title', 'author', 'description', 'year', 'publisher', 'pages']
     if current_user.is_admin():
         if request.method == 'POST':
-            book_data = {field: request.form.get(field) for field in fields}
+            book_data = {}
+            for field in fields:
+                book_data[field] = request.form[field]
             selected_genres = request.form.getlist('genres[]')
             cover_image = request.files.get('cover_image')
             cover_id = save_cover(cover_image)
+            book_data['cover_id'] = cover_id
             book_data['description'] = bleach.clean(book_data['description'])
 
             with db_connector.connect().cursor(named_tuple=True, buffered=True) as cursor:
                 cursor.execute("INSERT INTO books (title, description, year, publisher, author, pages, cover_id) VALUES"
-                               " (%(title)s, %(description)s, %(year)s, %(publisher)s, %(author)s, %(pages)s, %(cover_id)s)",
-                               {**book_data, 'cover_id': cover_id})
-                book_id = cursor.lastrowid
-                db_connector.connect().commit()
+                               " (%s, %s, %s, %s, %s, %s, %s)",
+                               (book_data['title'], book_data['description'], book_data['year'], book_data['publisher'],
+                                book_data['author'], book_data['pages'], book_data['cover_id']))
 
-            insert_book_genres(book_id, selected_genres)
+                for genre_id in selected_genres:
+                    cursor.execute("INSERT IGNORE INTO book_genres (book_id, genre_id) VALUES (%s, %s)",
+                                   (cursor.lastrowid, genre_id))
+
+                db_connector.connect().commit()
 
             flash("Книга успешно добавлена!", "success")
             return redirect(url_for('books.index'))
@@ -117,17 +113,24 @@ def edit(book_id):
     if current_user.is_admin() or current_user.is_moderator():
         if request.method == 'POST':
             fields = ['title', 'author', 'description', 'year', 'publisher', 'pages']
-            book_data = {field: request.form.get(field) for field in fields}
+            book_data = {}
+            for field in fields:
+                book_data[field] = request.form[field]
             selected_genres = request.form.getlist('genres[]')
             book_data['description'] = bleach.clean(book_data['description'])
 
             with db_connector.connect().cursor(named_tuple=True, buffered=True) as cursor:
-                cursor.execute("UPDATE books SET title = %(title)s, description = %(description)s, year = %(year)s, "
-                               "publisher = %(publisher)s, author = %(author)s, pages = %(pages)s WHERE id = %(book_id)s",
-                               {**book_data, 'book_id': book_id})
-                db_connector.connect().commit()
+                cursor.execute(
+                    "UPDATE books SET title = %s, description = %s, year = %s, "
+                    "publisher = %(s, author = %s, pages = %s WHERE id = %s",
+                    (book_data['title'], book_data['description'], book_data['year'], book_data['publisher'],
+                     book_data['author'], book_data['pages'], book_id))
 
-            insert_book_genres(book_id, selected_genres)
+                for genre_id in selected_genres:
+                    cursor.execute("INSERT IGNORE INTO book_genres (book_id, genre_id) VALUES (%s, %s)",
+                                   (book_id, genre_id))
+
+                db_connector.connect().commit()
 
             flash("Книга успешно обновлена!", "success")
             return redirect(url_for('books.view', book_id=book_id))
@@ -136,7 +139,7 @@ def edit(book_id):
         cursor.execute("SELECT * FROM books WHERE id = %s", (book_id,))
         book = cursor.fetchone()
 
-        if not book:
+        if book is None:
             flash("Книга не найдена.", "error")
             return redirect(url_for('books.index'))
 
@@ -147,7 +150,9 @@ def edit(book_id):
         selected_genres = cursor.fetchall()
 
     book_dict = book._asdict()
-    book_dict["selected_genres"] = [id[0] for id in selected_genres]
+    for id in selected_genres:
+        book_dict["selected_genres"] = id.genre_id
+
     return render_template('books/edit.html', genres=genres, book_data=book_dict, book_id=book_id)
 
 
@@ -172,15 +177,15 @@ def view(book_id):
         reviews = cursor.fetchall()
 
         cursor.execute("SELECT * FROM reviews WHERE user_id = %s and book_id = %s", (current_user.id, book_id))
-        is_commented = cursor.fetchone() is not None
+        is_commented_by_this_user = cursor.fetchone() is not None
 
-    if not book:
+    if book is None:
         flash("Книга не найдена", "warning")
         return redirect(url_for('books.index'))
 
     book_dict = book._asdict()
 
-    return render_template('books/view.html', book=book_dict, reviews=reviews, is_commented=is_commented)
+    return render_template('books/view.html', book=book_dict, reviews=reviews, is_commented=is_commented_by_this_user)
 
 
 @bp.route('/delete/<int:book_id>', methods=['POST', 'GET'])
@@ -194,12 +199,14 @@ def delete(book_id):
                 with connection.cursor(named_tuple=True, buffered=True) as cursor:
                     cursor.execute("SELECT * FROM books WHERE id = %s", (book_id,))
                     book = cursor.fetchone()
+
                     cursor.execute("SELECT filename FROM covers WHERE id = %s", (book.cover_id,))
                     cover = cursor.fetchone()
+
                     cursor.execute("SELECT COUNT(*) as count FROM books where cover_id = %s", (book.cover_id,))
                     cover_count = cursor.fetchone()
 
-                    if not book:
+                    if book is None:
                         flash("Книга не найдена", "warning")
                         return redirect(url_for('books.index'))
 
@@ -232,12 +239,19 @@ def add_review(book_id):
         try:
             connection = db_connector.connect()
             with connection.cursor(named_tuple=True, buffered=True) as cursor:
-                cursor.execute("INSERT INTO reviews(book_id, user_id, rating, review_text, date_added) "
-                               "VALUES (%s, %s, %s, %s, %s)",
-                               (book_id, current_user.id, request.form['rating'],
-                                request.form['review_text'], datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-            connection.commit()
-            flash('Рецензия успешно добавлена!', 'success')
+                cursor.execute("SELECT * from reviews WHERE user_id = %s and book_id = %s", (current_user.id, book_id))
+                count = cursor.fetchone()
+
+                if count is None:
+                    cursor.execute("INSERT INTO reviews(book_id, user_id, rating, review_text, date_added) "
+                                   "VALUES (%s, %s, %s, %s, %s)",
+                                   (book_id, current_user.id, request.form['rating'],
+                                    request.form['review_text'], datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                    flash('Рецензия успешно добавлена!', 'success')
+                    connection.commit()
+                else:
+                    flash('Вы уже добавляли реценизю на эту книгу', 'danger')
+
         except Exception as e:
             print(e)
             flash("Произошла ошибка при написании рецензии.", "danger")
